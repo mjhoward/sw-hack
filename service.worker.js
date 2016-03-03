7537faa56757727a9d121e2c2204866110a3807a
Abdel <aelbouhy@wiley.com>
2016-03-03 15:19:58 +0000

    master|Abdel|trying to fetch network first

--- service.worker.js
+++ service.worker.js
@@ -8,8 +8,8 @@ this.addEventListener('install', function(event) {
     caches.open('v1')
       .then(function(cache) {
         return cache.addAll([
-          baseUrl,
-          baseUrl + 'index.html'
+          localhost,
+          localhost + 'index.html'
         ]);
       })
       .then(function() {
@@ -24,39 +24,52 @@ this.addEventListener('install', function(event) {
 var network = false;
 
 this.addEventListener('fetch', function(event) {
-  var url = event.request.url;
-  var matcher = url.match(/https:\/\/.*\/(.*)/);
-  var path = matcher[1];
-  fetch(event.request).then(function(r) {
-    console.log('then after fetch', r);
-    network = true;
-    return r;
-  }).then(function(data) {
-    console.log('then with data', data);
-    event.waitUntil(
-      caches.open('v1')
-        .then(function(cache) {
-          console.log('adding path', path);
-          return cache.add(path);
-        })
-        .then(function() {
-          console.log('All URLs cached');
-        })
-        .catch(function(error) {
-          console.log('Cache failed:', error);
-        })
-    );
-  });
-
-  if (!network) {
-      event.respondWith(caches.match(event.request)).then(function(repsonse) {
-      console.log('responding', repsonse)
-      return caches.open('v1').then(function(cache) {
-        console.log('last cache')
-        return cache.match(path);
-      }).catch(function(error) {
-          console.log('from cache failed', error);
-        });
-    });
-  }
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
+      })
+    )
 });
+
+
+
+
+
+
+
+  // var url = event.request.url;
+  // var matcher = url.match(/https:\/\/.*\/(.*)/);
+  // var path = matcher[1];
+  // fetch(event.request).then(function(r) {
+  //   console.log('then after fetch', r);
+  //   network = true;
+  //   return r;
+  // })
+
+
+
+
+  // .then(function(data) {
+  //   console.log('then with data', data);
+  //   event.waitUntil(
+  //     caches.open('v1')
+  //       .then(function(cache) {
+  //         console.log('adding path', path);
+  //         return cache.add(path);
+  //       })
+  //       .then(function() {
+  //         console.log('All URLs cached');
+  //       })
+  //       .catch(function(error) {
+  //         console.log('Cache failed:', error);
+  //       })
+  //   );
+  // });
+
+  // caches.match(path).then(function(repsonse) {
+  //   return response
+  // }).then(function(data) {
+  //   if (!network) {
+  //      return cache.match(path);
+  //   }
+  // });
