09c2d7e47aa70e6655bd340a14fa5b46b1bb2b3a
Abdel <aelbouhy@wiley.com>
2016-03-04 11:53:48 +0000

    master|adding files

--- service.worker.js
+++ service.worker.js
@@ -1,16 +1,60 @@
+function addAllFiles() {
+  var isLocal = true;
+  var baseUrl = isLocal ? '/' : '/mjhoward/sw-hack/master/';
+
+  var arr = [baseUrl]
+
+  for (var i = 1; i < 19; i ++) {
+    (function(idx) {
+      var offset = (idx < 10) ? 0 : ''
+      var url =  baseUrl + 'assets/equation-neqn0' + offset + idx + '.gif'
+      arr.push(url)
+    })(i);
+  }
+
+  for (var i = 1; i < 6; i ++) {
+    (function(idx) {
+      var url =  baseUrl + 'figures/nfig00' + + idx + '.jpg'
+      arr.push(url)
+    })(i);
+  }
+
+  for (var i = 1; i < 19; i ++) {
+    (function(idx) {
+      var offset = (idx < 10) ? 0 : ''
+      var url =  baseUrl + 'assets/equation-nueq0' + offset + idx + '.gif'
+      arr.push(url)
+    })(i);
+  }
+
+  for (var i = 1; i < 51; i ++) {
+    (function(idx) {
+      var url =  baseUrl + 'assets/equation-tex2gif-stack-' + idx + '.gif'
+      arr.push(url)
+    })(i);
+  }
+
+  arr.push(baseUrl + 'assets/footer-logo.png');
+  arr.push(baseUrl + 'assets/header-logo.png');
+  arr.push(baseUrl + 'assets/wol-article.css');
+  arr.push(baseUrl + 'assets/wol-common.css');
+  arr.push(baseUrl + 'assets/cover.gif');
+  arr.push(baseUrl + 'assets/olalertbanner.jpg');
+
+  return arr;
+
+}
+
+
 this.addEventListener('install', function(event) {
   console.log('Install event');
-  var baseUrl = '/mjhoward/sw-hack/master/';
-  var localhost = '/';
-
+  var arr = addAllFiles();
+  console.log(arr)
 
   event.waitUntil(
     caches.open('v1')
       .then(function(cache) {
-        return cache.addAll([
-          baseUrl,
-          baseUrl + 'index.html'
-        ]);
+        return cache.addAll(arr);
       })
       .then(function() {
         console.log('All URLs cached');
