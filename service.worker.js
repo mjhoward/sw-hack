e2c60a54f7d12426daf3c40ea97a4bc824a25322
Mark Howard <mark.howard1@uk.ibm.com>
2017-01-26 22:39:54 +0000

    Reverting

--- service.worker.js
+++ service.worker.js
@@ -69,7 +69,8 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1')
       .then(function(cache) {
-        return cache.addAll(arr);
+        //return cache.addAll(arr);
+        return cache.addAll(['/mjhoward/sw-hack/master/index.html']);
       })
       .then(function() {
         console.log('All URLs cached');
